04246366e78541f56a2f98567472a1455d269403
inni-ast <inni_@tut.by>
2024-02-08 10:50:17 +0300

    feat: les02_task3

---
+++ lesson-02/03.js
@@ -0,0 +1,9 @@
+// code
+const giveMeRightNumbers = (value1, value2, maximum) => {
+  if (value1 === value2 || value1 <= maximum) {
+    console.log("Success!");
+  } else {
+    console.log("Something wrong!");
+  }
+};
+giveMeRightNumbers(30, 5, 20);
